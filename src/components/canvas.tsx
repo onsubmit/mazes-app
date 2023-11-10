c5cb14f552530621815fb851751a10fdd7ee79b8
Andy <onsubmit@outlook.com>
2023-11-09 20:05:05 -0800

    Add Grid.draw() method

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -11,79 +11,15 @@ export default function Canvas({ grid, cellSize = 32 }: CanvasProps) {
   const width = cellSize * grid.columns + 1;
   const height = cellSize * grid.rows + 1;
 
-  const backgroundColor = '#fff';
-  const backgroundColorEmptyCell = '#000';
-  const strokeStyle = '#000';
-
-  const ref = useRef<HTMLCanvasElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    if (!ref.current) {
+    if (!canvasRef.current) {
       return;
     }
 
-    const canvas = ref.current;
-    const context = canvas.getContext('2d');
-    if (!context) {
-      throw new Error('Could not get rendering context');
-    }
+    grid.draw(canvasRef.current, cellSize);
+  }, [cellSize, grid, canvasRef]);
 
-    context.fillStyle = backgroundColor;
-    context.strokeStyle = strokeStyle;
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    const drawLine = (x1: number, y1: number, x2: number, y2: number) => {
-      context.beginPath();
-      context.moveTo(x1, y1);
-      context.lineTo(x2, y2);
-      context.stroke();
-    };
-
-    const drawRectangle = (x1: number, y1: number, x2: number, y2: number, color: string) => {
-      context.fillStyle = color;
-      context.fillRect(x1, y1, x2, y2);
-      context.fillStyle = backgroundColor;
-    };
-
-    for (const mode of ['backgrounds', 'walls'] as const) {
-      grid.forEachCell(({ row, column, cell }) => {
-        const x1 = column * cellSize;
-        const y1 = row * cellSize;
-        const x2 = (column + 1) * cellSize;
-        const y2 = (row + 1) * cellSize;
-
-        switch (mode) {
-          case 'backgrounds': {
-            const color = cell.isEmpty
-              ? backgroundColorEmptyCell
-              : grid.getCellBackgroundColor(cell) ?? backgroundColor;
-            drawRectangle(x1, y1, x2, y2, color);
-
-            break;
-          }
-          case 'walls': {
-            if (!cell.north) {
-              drawLine(x1, y1, x2, y1);
-            }
-
-            if (!cell.west) {
-              drawLine(x1, y1, x1, y2);
-            }
-
-            if (!cell.isLinkedTo(cell.east)) {
-              drawLine(x2, y1, x2, y2);
-            }
-
-            if (!cell.isLinkedTo(cell.south)) {
-              drawLine(x1, y2, x2, y2);
-            }
-
-            break;
-          }
-        }
-      });
-    }
-  }, [cellSize, grid, ref]);
-
-  return <canvas ref={ref} width={width} height={height}></canvas>;
+  return <canvas ref={canvasRef} width={width} height={height}></canvas>;
 }
